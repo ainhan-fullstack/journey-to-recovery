b217e7cc3f04676da8c576dea6f404b2376cdfd7
ainhan-fullstack <nhanphamit96@gmail.com>
2025-11-30 19:23:05 +1030

    Deploy DB

--- packages/server/db/connection.ts
+++ packages/server/db/connection.ts
@@ -1,15 +1,18 @@
 import mysql from "mysql2/promise";
 import dbConfig from "../config/db.config";
 
-const pool = mysql.createPool({
-  host: dbConfig.HOST,
-  user: dbConfig.USER,
-  password: dbConfig.PASSWORD,
-  database: dbConfig.DB,
-  namedPlaceholders: true,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
+const urlDB = `mysql://root:uDYwwhWKNaPQtKquiLLaPlEnKKMCZLpn@maglev.proxy.rlwy.net:52221/railway`;
+
+const pool = mysql.createPool(urlDB);
+// const pool = mysql.createPool({
+//   host: dbConfig.HOST,
+//   user: dbConfig.USER,
+//   password: dbConfig.PASSWORD,
+//   database: dbConfig.DB,
+//   namedPlaceholders: true,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
 
 export default pool;
